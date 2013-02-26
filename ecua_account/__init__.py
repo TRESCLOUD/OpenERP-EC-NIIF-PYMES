ef572dd8d677265ae21b56b9eb9cca2894aee069
andrea <andrea@andrea-HP-ProBook-4530s>
2013-02-26 14:06:07 -0500

    ecua_account aumento para error de borrado de tablas

--- ecua_account/__init__.py
+++ ecua_account/__init__.py
@@ -22,6 +22,7 @@
 #along with this program.  If not, see http://www.gnu.org/licenses.
 ########################################################################
 
+import ecua_account_report
 import object
 import wizard
 import report
