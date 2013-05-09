708d4d439e199c3d305135a26cf09eccf2757a6d
carlos <carlos@ruth-HP-ProBook-4530s>
2013-05-09 16:04:03 -0500

    Módulo que permite cargar automaticamente lineas a dias trabajados y otros ingresos de la vista "Employee Payslips pestaña Worked Days & Inputs"

---
+++ ecua_hr/ecua_hr.py
@@ -0,0 +1,125 @@
+# -*- encoding: utf-8 -*-
+########################################################################
+#                                                                       
+# @authors: Carlos Yumbillo                                                                           
+# Copyright (C) 2013  TRESCLOUD Cia Ltda.                                 
+#                                                                       
+#This program is free software: you can redistribute it and/or modify   
+#it under the terms of the GNU General Public License as published by   
+#the Free Software Foundation, either version 3 of the License, or      
+#(at your option) any later version.                                    
+#
+# This module is GPLv3 or newer and incompatible
+# with OpenERP SA "AGPL + Private Use License"!
+#                                                                       
+#This program is distributed in the hope that it will be useful,        
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+#GNU General Public License for more details.                           
+#                                                                       
+#You should have received a copy of the GNU General Public License      
+#along with this program.  If not, see http://www.gnu.org/licenses.
+########################################################################
+
+from osv import osv, fields 
+from datetime import datetime
+import calendar
+
+class hr_payslip(osv.osv):
+    '''
+    Pay Slip
+    '''
+    _inherit = 'hr.payslip'
+
+    def get_inputs(self, cr, uid, contract_ids, date_from, date_to, context=None):
+        res = super(hr_payslip, self).get_inputs(cr, uid, contract_ids, date_from, date_to, context=context)
+        contract_obj = self.pool.get('hr.contract')
+
+        for contract in contract_obj.browse(cr, uid, contract_ids, context=context):
+
+            input1 = {
+                 'name': 'Bono',
+                 'code': 'BONIFICACIÓN',
+                 'amount': 0.0,
+                 'contract_id': contract.id,
+            }
+            input2 = {
+                 'name': 'Comisión',
+                 'code': 'COMISIÓN',
+                 'amount': 0.0,
+                 'contract_id': contract.id,
+            }
+            input3 = {
+                 'name': 'Transporte',
+                 'code': 'TRANSPORTE',
+                 'amount': 0.0,
+                 'contract_id': contract.id,
+            }
+            input4 = {
+                 'name': 'Alimentación',
+                 'code': 'ALIMENTACIÓN',
+                 'amount': 0.0,
+                 'contract_id': contract.id,
+            }
+            res += [input1,input2,input3,input4]
+
+        return res
+    
+    def get_worked_day_lines(self, cr, uid, contract_ids, date_from, date_to, context=None):
+        res = super(hr_payslip, self).get_worked_day_lines(cr, uid, contract_ids, date_from, date_to, context=context)
+        contract_obj = self.pool.get('hr.contract')
+        
+        if date_from:
+            DATETIME_FORMAT = "%Y-%m-%d"
+            date_from = datetime.strptime(date_from, DATETIME_FORMAT)
+            year = date_from.year
+            mes = date_from.month
+            
+            if mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12:
+               days = 31.0
+            elif mes==4 or mes==6 or mes==9 or mes==11:
+                 days = 30.0
+                    
+            if mes==2:
+                if calendar.isleap(year):
+                    days = 29.0
+                else: 
+                    days = 28.0
+        else:
+            days = 30.0
+        
+        for contract in contract_obj.browse(cr, uid, contract_ids, context=context):
+
+            attendances1 = {
+                 'name': 'Horas extras regulares',
+                 'code': 'HORA_EXTRA_REGULAR',
+                 'number_of_days': 0.0,
+                 'number_of_hours': 0.0,
+                 'contract_id': contract.id,
+            }
+            attendances2 = {
+                 'name': 'Horas extras extraordinarias',
+                 'code': 'HORA_EXTRA_EXTRAORDINARIA',
+                 'number_of_days': 0.0,
+                 'number_of_hours': 0.0,
+                 'contract_id': contract.id,
+            }
+            attendances3 = {
+                 'name': 'Dias Calendario del Mes',
+                 'code': 'DIAS_DEL_MES',
+                 'number_of_days': days,
+                 'number_of_hours': 0.0,
+                 'contract_id': contract.id,
+            }
+            attendances4 = {
+                 'name': 'Dias Calendario Laborados',
+                 'code': 'DIAS_TRABAJADOS',
+                 'number_of_days': days,
+                 'number_of_hours': 0.0,
+                 'contract_id': contract.id,
+            }  
+            res += [attendances1,attendances2,attendances3,attendances4]
+
+        return res  
+            
+hr_payslip()
