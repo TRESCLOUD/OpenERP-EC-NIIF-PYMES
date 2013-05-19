613a7d3ddfb6fa97992785df75194cf12fc56cf9
carlos <carlos.yumbillo@trescloud.com>
2013-05-19 12:46:03 -0500

    Módulo para administrar el tipo de documento.

---
+++ ecua_invoice_type/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#                                                                       
+# @authors:TRESCLOUD Cia.Ltda                                                                           
+# Copyright (C) 2013                                  
+#                                                                       
+#This program is free software: you can redistribute it and/or modify   
+#it under the terms of the GNU General Public License as published by   
+#the Free Software Foundation, either version 3 of the License, or      
+#(at your option) any later version.                                    
+#
+# This module is GPLv3 or newer and incompatible
+# with OpenERP SA "AGPL + Private Use License"!
+#                                                                       
+#This program is distributed in the hope that it will be useful,        
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+#GNU General Public License for more details.                           
+#                                                                       
+#You should have received a copy of the GNU General Public License      
+#along with this program.  If not, see http://www.gnu.org/licenses.
+#ice
+########################################################################
+{
+   "name" : "Módulo de facturación a proveedores/clientes para Ecuador",
+   "author" : "TRESCloud Cia. Ltda.",
+   "maintainer": 'TRESCloud Cia. Ltda.',
+   "website": 'http://www.trescloud.com',
+   'complexity': "easy",
+   "description": """Sistema de gestión y control de compras y ventas 
+   
+   Este sistema permite el control del tipo de documento autorizado a emitir al momento de realizar una compra o venta, por ejemplo (Factura, nota de venta, liquidación de compra, etc).
+   Nota:
+   Si no se va a utilizar el módulo de contabilidad ecuatoriana "ecua_facturas_manual", borrarla de depends, descomentar las lineas (101 a la 116) y comentar (83 a la 100) del archivo ecua_invoice_type.xml 
+     
+   Desarrollador:
+   
+   Carlos Yumbillo
+   
+   """,
+   "category": "Contabilidad",
+   "version" : "1.0",
+   'depends': ['base','account','ecua_facturas_manual','ecua_facturas_ice_manual'],
+   'init_xml': [],
+   'update_xml': [
+       'report/report.xml',
+       'data/document_type.xml',
+       'ecua_invoice_type.xml',       
+   ],
+   'installable': True,
+}
