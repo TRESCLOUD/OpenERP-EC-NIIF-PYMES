3d6b4ea1024331b02d17561c7f3fe41765cd453c
andrea <andrea@andrea-HP-ProBook-4530s>
2013-08-15 11:01:51 -0500

    MODIFICACION DE CAMPOS Y CRACION DE NUEVOS PARA EL CALCULO DE FONDOS DE RESERVA

--- ecua_hr/ecua_hr.py
+++ ecua_hr/ecua_hr.py
@@ -116,7 +116,35 @@ class hr_payslip(osv.osv):
                 cr.execute('''delete from hr_payslip_input where payslip_id=%s''' %(payslips_line.id))
                 
         return super(hr_payslip, self).unlink(cr, uid, ids)
-            
+    
+    def _compute_year(self, cr, uid, ids, field, arg, context=None):
+        ''' Función que calcula el número de años de servicio de un empleado trabajando para la empresa.'''
+        
+        res = {}
+        DATETIME_FORMAT = "%Y-%m-%d"
+        #today = datetime.now()
+
+        for payslip in self.browse(cr, uid, ids, context=context):
+            if payslip.date_to and payslip.contract_id.date_start:               
+                date_start = datetime.strptime(payslip.contract_id.date_start, DATETIME_FORMAT)
+                today=datetime.strptime(payslip.date_to, DATETIME_FORMAT)
+                diffyears = today.year - date_start.year
+                difference = today - date_start.replace(today.year)
+                days_in_year = calendar.isleap(today.year) and 366 or 365
+                difference_in_years = diffyears + (difference.days + difference.seconds / 86400.0) / days_in_year
+                total_years = relativedelta(today, date_start).years
+                total_months = relativedelta(today, date_start).months
+                months_in_years = total_months*0.083333333
+                year_month = float(total_months) / 100 + total_years
+                number_of_year = total_years + months_in_years  
+                res[payslip.id] = number_of_year
+            else:
+                res[payslip.id] = 0.0
+        return res
+    _columns={
+              'number_of_year': fields.function(_compute_year, string='No. of years of service', type='float', store=False, method=True, help='Total years of work experience'),
+              }
+         
 hr_payslip()
 
 class hr_contract(osv.osv):
@@ -147,9 +175,24 @@ class hr_contract(osv.osv):
                 res[contract.id] = 0.0
         return res
     
+    def _compute_day_pay(self, cr, uid, ids, field, arg, context=None):
+        """Funcion que me ayuda para calcular los fondos de reserva del empleado"""
+        res = {}
+        DATETIME_FORMAT = "%Y-%m-%d"
+        today = datetime.now()
+        last_day=30
+        for contract in self.browse(cr, uid, ids, context=context):
+            date_start = datetime.strptime(contract.date_start, DATETIME_FORMAT)
+            if date_start.day<last_day and date_start.day>1:
+                res[contract.id]=last_day-date_start.day
+            else:
+                res[contract.id] = last_day
+        return res    
+    
     _columns = {
        'number_of_year': fields.function(_compute_year, string='No. of years of service', type='float', store=False, method=True, help='Total years of work experience'),
        'type_id': fields.many2one('hr.contract.type', "Contract Type", required=False),
+       'day_pay':fields.function(_compute_day_pay,string="Day Pay",type='float',store=False,method=True),
         }
     
     _defaults = {
