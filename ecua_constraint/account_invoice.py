83d1d4d88c33269ea2f2933ddcf92bb3d2a67302
andrea <andrea@andrea-HP-ProBook-4530s>
2013-10-29 15:01:14 -0500

    se cambio a español el mensaje de error

--- ecua_constraint/account_invoice.py
+++ ecua_constraint/account_invoice.py
@@ -12,7 +12,7 @@ class account_invoice(osv.osv):
     _inherit = "account.invoice"
     
     _sql_constraints = [
-                        ('invoice_number_out_not_uniq','unique(invoice_number_out)', _("There's another invoice with this number!")),
+                        ('invoice_number_out_not_uniq','unique(invoice_number_out)', _("Existe otra factura con el mismo numero de factura!")),
                         ('number_credit_note_uniq','unique(number_credit_note_out)', _("There's another credit note with this number!")),
                         
                         ]
