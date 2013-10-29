22cd5f19d2d01e235d2d57ae19864a57c100bb1a
andrea <andrea@andrea-HP-ProBook-4530s>
2013-10-29 13:49:31 -0500

    correccion de los dos constraints

--- ecua_constraint/account_invoice.py
+++ ecua_constraint/account_invoice.py
@@ -12,7 +12,8 @@ class account_invoice(osv.osv):
     _inherit = "account.invoice"
     
     _sql_constraints = [
-                        ('invoice_number_out_not_uniq','unique(invoice_number_out)', _("There's another credit note with this number!")),
-                                                
+                        ('invoice_number_out_not_uniq','unique(invoice_number_out)', _("There's another invoice with this number!")),
+                        ('number_credit_note_uniq','unique(number_credit_note_out)', _("There's another credit note with this number!")),
+                        
                         ]
 account_invoice()
